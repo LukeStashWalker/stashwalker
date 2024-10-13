291f4eeab65a6acf5ccd19580dd78361fcd28d1b
Luke Stashwalker <original_plan_c@hotmail.com>
2024-10-13 14:13:18 +0200

    Update build script.

--- build.sh
+++ build.sh
@@ -7,6 +7,14 @@
 SOURCE_DIR="./build/libs/"
 DEST_DIR="$APPDATA/.minecraft/mods/"
 
+# Delete any files containing 'stashwalker' in the destination directory
+for file in "$DEST_DIR"*; do
+  if [[ "$file" == *stashwalker* ]]; then
+    rm "$file"
+    echo "Deleted $file"
+  fi
+done
+
 # Copy files from SOURCE_DIR to DEST_DIR, excluding those with 'sources' in the filename
 for file in "$SOURCE_DIR"*; do
   if [[ "$file" != *sources* ]]; then
